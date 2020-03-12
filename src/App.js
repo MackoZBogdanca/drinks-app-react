5c51b59f54fe954bc4e2417b0e4ccbd821aeaba7
MackoZBogdanca <mackozbogdanca@yandex.com>
2020-03-12 20:24:38 +0100

    drinks app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Recipe from "./compoents/Recipe";
 import "./App.css";
 
 const App = _ => {
-  const APP_ID = "ffe426ff";
-  const APP_KEY = "428e44b94812d8560d2f1237a2a178dd";
+  // const APP_ID = "ffe426ff";
+  // const APP_KEY = "428e44b94812d8560d2f1237a2a178dd";
+  const APP_KEY = "1";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("margarita");
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     getRecipes();
@@ -16,10 +18,12 @@ const App = _ => {
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      // `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      `https://www.thecocktaildb.com/api/json/v1/${APP_KEY}/search.php?s=${query}`
     );
     const data = await response.json();
-    setRecipes(data.hits);
+    setRecipes(data.drinks);
+    setLoading(false);
   };
 
   const updateSearch = e => {
@@ -32,32 +36,87 @@ const App = _ => {
     setSearch("");
   };
 
-  return (
-    <div className="app">
-      <form className="search-form" onSubmit={getSearch}>
-        <input
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipes">
-        {recipes.map(recipe => (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-            ingredients={recipe.recipe.ingredients}
+  // console.log(recipes);
+
+  if (loading) {
+    return (
+      <div className="app">
+        <h1 className="title">Search drinks app</h1>
+        <form className="search-form" onSubmit={getSearch}>
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+            placeholder="Search by ingredient"
           />
-        ))}
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <h1 className="error">Loading...</h1>
       </div>
-    </div>
-  );
+    );
+  } else if (recipes) {
+    return (
+      <div className="app">
+        <h1 className="title">Search drinks app</h1>
+        <form className="search-form" onSubmit={getSearch}>
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+            placeholder="Search by ingredient"
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="recipes">
+          {recipes.map(recipe => (
+            <Recipe
+              // key={recipe.recipe.label}
+              title={recipe.strDrink}
+              img={recipe.strDrinkThumb}
+              glass={recipe.strGlass}
+              instructions={recipe.strInstructions}
+              ingredient1={recipe.strIngredient1}
+              ingredient2={recipe.strIngredient2}
+              ingredient3={recipe.strIngredient3}
+              ingredient4={recipe.strIngredient4}
+              ingredient5={recipe.strIngredient5}
+              ingredient6={recipe.strIngredient6}
+              ingredient7={recipe.strIngredient7}
+              ingredient8={recipe.strIngredient8}
+              // calories={recipe.recipe.calories}
+              // image={recipe.recipe.image}
+              // ingredients={recipe.recipe.ingredients}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="app">
+        <h1 className="title">Search drinks app</h1>
+        <form className="search-form" onSubmit={getSearch}>
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+            placeholder="Search by ingredient"
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <h1 className="error">No results, try again...</h1>
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/compoents/Recipe.js
+++ src/compoents/Recipe.js
@@ -1,17 +1,57 @@
 import React from "react";
 import style from "./recipe.module.css";
 
-const Recipe = ({ title, calories, image, ingredients }) => {
+const Recipe = ({
+  title,
+  calories,
+  image,
+  ingredients,
+  img,
+  glass,
+  instructions,
+  ingredient1,
+  ingredient2,
+  ingredient3,
+  ingredient4,
+  ingredient5,
+  ingredient6,
+  ingredient7,
+  ingredient8
+}) => {
   return (
-    <div className={style.recipe}>
-      <h1>{title}</h1>
-      <ol>
-        {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ol>
-      <p>{calories}</p>
-      <img className={style.image} src={image} alt="" />
+    // <div className={style.recipe}>
+    //   <h1>{title}</h1>
+    //   <ol>
+    //     {ingredients.map(ingredient => (
+    //       <li>{ingredient.text}</li>
+    //     ))}
+    //   </ol>
+    //   <p>{calories}</p>
+    //   <img className={style.image} src={image} alt="" />
+    // </div>
+    <div className={style.recipes}>
+      <div className={style.recipe}>
+        <h1 className={style.name}>{title}</h1>
+        <img src={img} alt="" className={style.image}></img>
+      </div>
+      <div className={style.info}>
+        <h4>Ingredients:</h4>
+        <ul className={style.list}>
+          <li>{ingredient1}</li>
+          <li>{ingredient2}</li>
+          <li>{ingredient3}</li>
+          <li>{ingredient4}</li>
+          <li>{ingredient5}</li>
+          <li>{ingredient6}</li>
+          <li>{ingredient7}</li>
+          <li>{ingredient8}</li>
+        </ul>
+        <p>{instructions}</p>
+        <h4>
+          Recommended glass:
+          <br /> {glass}
+        </h4>
+      </div>
     </div>
   );
 };
